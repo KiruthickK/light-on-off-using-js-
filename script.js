6ff5e1d9bfe25bf07a00514ecb50492966303264
Bhuvaneshwaran K <97341390+bhuvid@users.noreply.github.com>
2023-03-04 19:35:08 +0530

    Some user interactions added

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-var b1=document.getElementById("btn");
-var b2=document.getElementById("l1")
-function togglebtn(){
+window.onload = (()=>{
+    doTHisOnLoad();
+});
+const doTHisOnLoad = (() => {
+    var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
+    myModal.show();
+})
+var b1 = document.getElementById("btn");
+var b2 = document.getElementById("l1")
+function togglebtn() {
     b1.classList.toggle("active");
     b2.classList.toggle("on");
 }
